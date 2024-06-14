a07218c359b7ea56e2f6c1d0d9cf64f57017c011
msabhinavchandra <msabhinavchandra@gmail.com>
2024-06-14 16:22:43 +0530

    Comments_For_KMIT_Officials_GitHub_Repo

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutterbasics/DashBoardScreen.dart';
 import 'package:flutterbasics/Settings.dart';
 import 'package:flutterbasics/Speech_To_Text.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:flutter/foundation.dart';
 import 'firebase_options.dart';
@@ -16,14 +15,17 @@ import 'dart:io';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  //Ensures that Flutter has been fully initialized before running the app.
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+  //Initializes Firebase with the provided options
   if (kDebugMode) {
-    const host = '192.168.29.240';
+    //Configures Firebase Functions to use a local emulator if
+    //the app is running in debug mode.
+    const host = '192.168.240.168';
     FirebaseFunctions.instanceFor(region: "us-central1")
         .useFunctionsEmulator(host, 5001);
   }
-  runApp(const MyApp());
+  runApp(const MyApp()); //Launches the root widget of the application
 }
 
 class MyApp extends StatelessWidget {
@@ -32,21 +34,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
+      //Provides the AppState to the widget tree,
+      //allowing state management.
       create: (_) => AppState(),
       child: MaterialApp(
+        //Sets up the material design for the app,
+        //including themes and the home page.
         debugShowCheckedModeBanner: false,
         title: "Vision Crafters",
         theme: ThemeData.light(),
         darkTheme: ThemeData.dark(),
         themeMode: ThemeMode.system,
         home: const HomePage(),
+        //The initial screen displayed when the app starts.
       ),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  //A stateful widget that maintains the state of the home page.
+
+  const HomePage({super.key}); //constructor for the HomePage widget
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -54,13 +63,16 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<String> descriptions = [];
+  //used for List of text descriptions generated from images or videos
   File? imageGal;
   File? imageCam;
-  final ImagePicker _pickerGal = ImagePicker();
-  final ImagePicker _pickerCam = ImagePicker();
+  //Files for selected images from the gallery and camera
+
   bool showSpinner = false;
+  //Boolean to control the display of a loading spinner.
 
   void addDescription(String description) {
+    //Adds a new description to the list and updates the state of the widget.
     setState(() {
       descriptions.add(description);
     });
@@ -68,9 +80,11 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    //Builds the UI of the home page
     final appState = Provider.of<AppState>(context);
     return Scaffold(
       appBar: AppBar(
+        //Displays the title and a settings button.
         title: const Text("Vision Crafters"),
         actions: [
           IconButton(
@@ -85,64 +99,93 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       drawer: Drawer(
+        //Provides a navigation drawer
         width: MediaQuery.of(context).size.width * 0.8,
         child: DashBoardScreen(),
       ),
       body: ModalProgressHUD(
+        //Displays a loading spinner when appState.showSpinner is true
         inAsyncCall: appState.showSpinner,
         child: Column(
           children: [
+            //Displays the UI of the home page
             const Center(
               child: Text(
                 "Welcome to Vision Crafters",
+                //Displays a welcome message.
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
             ),
             Expanded(
               child: ListView.builder(
-                itemCount: descriptions.length,
+                //Displays the list of descriptions using ListView.builder
+                itemCount: descriptions.length, // Number of items in the list
                 itemBuilder: (context, index) {
+                  // Builds each item in the list
                   return ListTile(
+                    // Displays each item in the list as a ListTile
                     title: Text(descriptions[index]),
                   );
                 },
               ),
             ),
             Container(
-              padding: const EdgeInsets.all(8.0),
+              // Displays a floating action button
+              padding: const EdgeInsets.all(8.0), // Padding around the button
               child: Row(
+                //Contains a speed dial for image/video picking,a text
+                //input field, and a microphone button for speech-to-text
                 children: [
                   FloatingActionButton(
+                    //Displays a speed dial for image/video picking
                     shape: const CircleBorder(),
-                    heroTag: "UniqueTag2",
+                    heroTag: "UniqueTag2", //Unique identifier for the button
                     onPressed: () {},
                     child: SpeedDial(
-                      animatedIcon: AnimatedIcons.menu_close,
-                      direction: SpeedDialDirection.up,
+                      //Speed dial for image/video picking
+                      animatedIcon: AnimatedIcons
+                          .menu_close, //Animated icon for the button
+                      direction:
+                          SpeedDialDirection.up, //Direction of the speed dial
                       children: [
+                        //List of children for the speed dial
                         SpeedDialChild(
-                          shape: const CircleBorder(),
-                          child: const Icon(Icons.camera),
+                          //Child for the camera button
+                          shape: const CircleBorder(), //Shape of the button
+                          child: const Icon(Icons.camera), //Icon for the button
                           onTap: () =>
                               getImageCM(context, addDescription, appState),
+                          //Function to be executed when the button is pressed
+                          //Calls the getImageCM function with the context,
+                          //addDescription, and appState as parameters
                         ),
                         SpeedDialChild(
+                          //Child for the video button
                           shape: const CircleBorder(),
                           child: const Icon(Icons.video_call),
                           onTap: () =>
                               getVideoFile(context, addDescription, appState),
+                          // Function to be executed when the button is pressed
+                          //Calls the getVideoFile function with the context,
+                          //addDescription, and appState as parameters
                         ),
                         SpeedDialChild(
+                          //Child for the gallery button
                           shape: const CircleBorder(),
                           child: const Icon(Icons.browse_gallery_sharp),
                           onTap: () =>
                               pickMedia(context, addDescription, appState),
+                          //Function to be executed when the button is pressed
+                          //Calls the pickMedia function with the context,
+                          //addDescription, and appState as parameters
                         ),
                       ],
                     ),
                   ),
                   Expanded(
+                    //Expanded widget to expand the text input field
                     child: Padding(
+                      //Padding widget to add padding to the text input field
                       padding: const EdgeInsets.symmetric(horizontal: 8.0),
                       child: TextFormField(
                         decoration: InputDecoration(
@@ -159,21 +202,29 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                   FloatingActionButton(
+                    //Displays a microphone button for speech-to-text
                     onPressed: () {
+                      //Function to be executed when the button is pressed
                       showDialog(
-                        context: context,
-                        builder: (context) => const Speech(),
+                        //Displays a dialog box for speech-to-text
+                        context: context, //Context for the dialog box
+                        builder: (context) =>
+                            const Speech(), //Speech dialog box
+                        // will be opened up when the microphone button is pressed
                       );
                     },
-                    child: const Icon(Icons.mic),
+                    child:
+                        const Icon(Icons.mic), // Icon for the microphone button
                   ),
-                ],
+                ], //end of children
               ),
             ),
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      //This places the FAB at the center of the bottom of the screen, docked 
+      //within the BottomAppBar.
     );
   }
 }
