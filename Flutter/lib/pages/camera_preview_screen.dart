7e344c4255d69eec2ed85d5d8b8ff2f73fb4952f
msabhinavchandra <msabhinavchandra@gmail.com>
2024-07-11 15:12:01 +0530

    q

--- Flutter/lib/pages/camera_preview_screen.dart
+++ Flutter/lib/pages/camera_preview_screen.dart
@@ -1,263 +1,63 @@
-import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'package:volume_watcher/volume_watcher.dart';
+import 'package:flutter/material.dart';
+import 'dart:developer' as developer;
 
 class CameraPreviewScreen extends StatefulWidget {
   final CameraController controller;
 
-  const CameraPreviewScreen({Key? key, required this.controller}) : super(key: key);
+  const CameraPreviewScreen({super.key, required this.controller});
 
   @override
-  _CameraPreviewScreenState createState() => _CameraPreviewScreenState();
+  CameraPreviewScreenState createState() => CameraPreviewScreenState();
 }
 
-class _CameraPreviewScreenState extends State<CameraPreviewScreen> {
-  bool _isVolumeButtonPressed = false;
+class CameraPreviewScreenState extends State<CameraPreviewScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
 
   @override
   void initState() {
     super.initState();
-    VolumeWatcher.addListener(_onVolumeButtonPressed);
-    widget.controller.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
-    });
+    _controller = widget.controller;
+    _initializeControllerFuture = _controller.initialize();
   }
 
   @override
   void dispose() {
-    VolumeWatcher.removeListener(_onVolumeButtonPressed);
-    widget.controller.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
-  void _onVolumeButtonPressed(int? volume) async {
-    if (volume != null && volume > 0 && !_isVolumeButtonPressed) {
-      setState(() {
-        _isVolumeButtonPressed = true;
-      });
-      await _captureImage();
-      setState(() {
-        _isVolumeButtonPressed = false;
-      });
-    }
-  }
-
-  Future<void> _captureImage() async {
+  Future<void> _takePicture() async {
     try {
-      final image = await widget.controller.takePicture();
-      Navigator.pop(context, image);
+      await _initializeControllerFuture;
+
+      XFile picture = await _controller.takePicture();
+
+      Navigator.pop(context, picture);
     } catch (e) {
-      print(e);
+      developer.log(e.toString());
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!widget.controller.value.isInitialized) {
-      return Container();
-    }
     return Scaffold(
-      body: Stack(
-        children: [
-          CameraPreview(widget.controller),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: FloatingActionButton(
-                child: Icon(Icons.camera),
-                onPressed: _captureImage,
-              ),
-            ),
-          ),
-        ],
+      appBar: AppBar(title: const Text('Take a Picture')),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(_controller);
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _takePicture,
+        child: const Icon(Icons.camera_alt),
       ),
     );
   }
 }
-
-
-// import 'package:flutter/material.dart';
-// import 'package:camera/camera.dart';
-// import 'package:volume_watcher/volume_watcher.dart';
-
-// class CameraPreviewScreen extends StatefulWidget {
-//   final CameraController controller;
-
-//   const CameraPreviewScreen({Key? key, required this.controller}) : super(key: key);
-
-//   @override
-//   _CameraPreviewScreenState createState() => _CameraPreviewScreenState();
-// }
-
-// class _CameraPreviewScreenState extends State<CameraPreviewScreen> {
-//   bool _isVolumeButtonPressed = false;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     VolumeWatcher.addListener(_onVolumeButtonPressed);
-//     widget.controller.initialize().then((_) {
-//       if (!mounted) {
-//         return;
-//       }
-//       setState(() {});
-//     });
-//   }
-
-//   @override
-//   void dispose() {
-//     VolumeWatcher.removeListener(_onVolumeButtonPressed);
-//     widget.controller.dispose();
-//     super.dispose();
-//   }
-
-//   void _onVolumeButtonPressed(double volume) async {
-//     if (volume > 0 && !_isVolumeButtonPressed) {
-//       setState(() {
-//         _isVolumeButtonPressed = true;
-//       });
-//       await _captureImage();
-//       setState(() {
-//         _isVolumeButtonPressed = false;
-//       });
-//     }
-//   }
-
-//   Future<void> _captureImage() async {
-//     try {
-//       final image = await widget.controller.takePicture();
-//       Navigator.pop(context, image);
-//     } catch (e) {
-//       print(e);
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     if (!widget.controller.value.isInitialized) {
-//       return Container();
-//     }
-//     return Scaffold(
-//       body: Stack(
-//         children: [
-//           CameraPreview(widget.controller),
-//           Align(
-//             alignment: Alignment.bottomCenter,
-//             child: Padding(
-//               padding: const EdgeInsets.all(20.0),
-//               child: FloatingActionButton(
-//                 child: Icon(Icons.camera),
-//                 onPressed: _captureImage,
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-
-
-
-
-
-
-// import 'package:camera/camera.dart';
-// import 'package:flutter/material.dart';
-// import 'dart:developer' as developer;
-// import 'package:volume_watcher/volume_watcher.dart';
-// class CameraPreviewScreen extends StatefulWidget {
-//   final CameraController controller;
-
-//   const CameraPreviewScreen({Key? key, required this.controller})
-//       : super(key: key);
-//   @override
-//   CameraPreviewScreenState createState() => CameraPreviewScreenState();
-// }
-
-// class CameraPreviewScreenState extends State<CameraPreviewScreen> {
-//   late CameraController _controller;
-//   late Future<void> _initializeControllerFuture;
-//   bool _isVolumeButtonPressed = false;
-
-//   @override
-//    void initState() {
-//     super.initState();
-//     VolumeWatcher.addListener(_onVolumeButtonPressed);
-//     widget.controller.initialize().then((_) {
-//       if (!mounted) {
-//         return;
-//       }
-//       setState(() {});
-//     });
-//   }
-
-//   @override
-//   void dispose() {
-//     VolumeWatcher.removeListener(_onVolumeButtonPressed);
-//     widget.controller.dispose();
-//     super.dispose();
-//   }
-
-//     void _onVolumeButtonPressed(VolumeWatcherEvent event) async {
-//     if (event.isVolumeUp && !_isVolumeButtonPressed) {
-//       setState(() {
-//         _isVolumeButtonPressed = true;
-//       });
-//       await _captureImage();
-//       setState(() {
-//         _isVolumeButtonPressed = false;
-//       });
-//     }
-//   }
-
-//     Future<void> _captureImage() async {
-//     try {
-//       final image = await widget.controller.takePicture();
-//       Navigator.pop(context, image);
-//     } catch (e) {
-//       print(e);
-//     }
-//   }
-
-//   // Future<void> _takePicture() async {
-//   //   try {
-//   //     await _initializeControllerFuture;
-
-//   //     XFile picture = await _controller.takePicture();
-
-//   //     Navigator.pop(context, picture);
-//   //   } catch (e) {
-//   //     developer.log(e.toString());
-//   //   }
-//   // }
-
-//  @override
-//   Widget build(BuildContext context) {
-//     if (!widget.controller.value.isInitialized) {
-//       return Container();
-//     }
-//     return Scaffold(
-//       body: Stack(
-//         children: [
-//           CameraPreview(widget.controller),
-//           Align(
-//             alignment: Alignment.bottomCenter,
-//             child: Padding(
-//               padding: const EdgeInsets.all(20.0),
-//               child: FloatingActionButton(
-//                 child: Icon(Icons.camera),
-//                 onPressed: _captureImage,
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
