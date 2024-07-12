03bc305ab0596eba20919fc7207f3474ac15d0eb
msabhinavchandra <msabhinavchandra@gmail.com>
2024-07-12 16:44:08 +0530

    DoubleTapSetForCapturingImage

--- Flutter/lib/pages/camera_preview_screen.dart
+++ Flutter/lib/pages/camera_preview_screen.dart
@@ -48,7 +48,10 @@ class CameraPreviewScreenState extends State<CameraPreviewScreen> {
         future: _initializeControllerFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            return CameraPreview(_controller);
+            return GestureDetector(
+              onDoubleTap: _takePicture,
+              child: CameraPreview(_controller),
+            );
           } else {
             return const Center(child: CircularProgressIndicator());
           }
@@ -61,3 +64,73 @@ class CameraPreviewScreenState extends State<CameraPreviewScreen> {
     );
   }
 }
+
+
+
+
+
+
+
+// import 'package:camera/camera.dart';
+// import 'package:flutter/material.dart';
+// import 'dart:developer' as developer;
+
+// class CameraPreviewScreen extends StatefulWidget {
+//   final CameraController controller;
+
+//   const CameraPreviewScreen({super.key, required this.controller});
+
+//   @override
+//   CameraPreviewScreenState createState() => CameraPreviewScreenState();
+// }
+
+// class CameraPreviewScreenState extends State<CameraPreviewScreen> {
+//   late CameraController _controller;
+//   late Future<void> _initializeControllerFuture;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _controller = widget.controller;
+//     _initializeControllerFuture = _controller.initialize();
+//   }
+
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+
+//   Future<void> _takePicture() async {
+//     try {
+//       await _initializeControllerFuture;
+
+//       XFile picture = await _controller.takePicture();
+
+//       Navigator.pop(context, picture);
+//     } catch (e) {
+//       developer.log(e.toString());
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Take a Picture')),
+//       body: FutureBuilder<void>(
+//         future: _initializeControllerFuture,
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.done) {
+//             return CameraPreview(_controller);
+//           } else {
+//             return const Center(child: CircularProgressIndicator());
+//           }
+//         },
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _takePicture,
+//         child: const Icon(Icons.camera_alt),
+//       ),
+//     );
+//   }
+// }
